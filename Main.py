93fefe65bd14562976206f26baec2b8664648cd7
Matt <lumpychum@gmail.com>
2023-02-23 17:36:24 -0600

    finished debugging main

--- Main.py
+++ Main.py
@@ -2,22 +2,27 @@ import Crawler
 from API import GoogleSheets
 import pandas as pd
 
-# Runs hourly to check for updates and serve them to the spreadsheet
+# Run hourly to check for updates and serve them to the spreadsheet
 def check_for_updates(spreadsheet, workbook_name):
     dao = GoogleSheets.DataObject()
 
     local_data = dao.pull_data(spreadsheet, workbook_name)  # data from Spreadsheet
     server_data = Crawler.fetch(write= True)  # data from AirBNB server
 
-    diff = len(local_data.compare(server_data))
+    try:
+        diff = len(local_data.compare(server_data))
 
-    if diff != 0:
-        print('Change found!')
+        if diff != 0:
+            print('Value change found!')
+            dao.push_data(spreadsheet, workbook_name)
+
+        else:
+            print('No change found.')
+
+    except Exception as e:
+        print('Structure change found! (error thrown)')
         dao.push_data(spreadsheet, workbook_name)
 
-    else:
-        print('No change yet...')
-
 spreadsheet = '1vVn2PuJybnMyO81SA4e1tVUGmPeSoRRd1LEbmU6jydk'
-workbook = 'remote'
+workbook = 'available_listings'
 check_for_updates(spreadsheet, workbook)
