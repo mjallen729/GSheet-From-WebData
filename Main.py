6903c8173cab5d311c2495fb1cd64691ff53d4c9
Matt <lumpychum@gmail.com>
2023-02-23 18:53:12 -0600

    moved link param to main

--- Main.py
+++ Main.py
@@ -3,11 +3,11 @@ from API import GoogleSheets
 import pandas as pd
 
 # Run hourly to check for updates and serve them to the spreadsheet
-def check_for_updates(spreadsheet, workbook_name):
+def check_for_updates(spreadsheet, workbook_name, link):
     dao = GoogleSheets.DataObject()
 
     local_data = dao.pull_data(spreadsheet, workbook_name)  # data from Spreadsheet
-    server_data = Crawler.fetch(write= True)  # data from AirBNB server
+    server_data = Crawler.fetch(link, write= True)  # data from AirBNB server
 
     try:
         diff = len(local_data.compare(server_data))
@@ -24,5 +24,7 @@ def check_for_updates(spreadsheet, workbook_name):
         dao.push_data(spreadsheet, workbook_name)
 
 spreadsheet = '1vVn2PuJybnMyO81SA4e1tVUGmPeSoRRd1LEbmU6jydk'
-workbook = 'available_listings'
-check_for_updates(spreadsheet, workbook)
+workbook = 'remote'
+wishlist_link = 'https://www.airbnb.com/wishlists/v/1250433647?s=67&unique_share_id=26a5887d-15af-4bc3-92ef-fa74ea9da81f'
+
+check_for_updates(spreadsheet, workbook, wishlist_link)
